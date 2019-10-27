fd643e27e77c9005a4e64dc3cca169905d91a636
Kyle Wuellner <rockstar81@comcast.net>
2019-10-26 23:23:30 -0500

    changed file locations

--- models/index.js
+++ models/index.js
@@ -1,14 +1,3 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const bookModel = new Schema({
-    title: { type: String, required: true },
-    authors: [{ type: String, required: true }],
-    description: String,
-    image: { type: String, trim: true },
-    link: { type: String, trim: true },
-});
-
-const Book = mongoose.model("Book", bookModel);
-
-module.exports = Book;
+module.exports = {
+    Book: require("./book")
+};
