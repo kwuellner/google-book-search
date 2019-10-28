cc72df8e2a3ebe8a2ba444ae19ac5c8daa3ecdf0
Kyle Wuellner <rockstar81@comcast.net>
2019-10-28 00:16:37 -0500

    added funtions in book.js

---
+++ client/build/precache-manifest.ddfaf3ad6a2679d7fe5af34f6175c6d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16a9346f93aa9602baff8b143a5bc32",
+    "url": "/kwuellner/google-book-search/index.html"
+  },
+  {
+    "revision": "b3a5d5d7e01829a9ac40",
+    "url": "/kwuellner/google-book-search/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a6c8da8fa2885f6b22d7",
+    "url": "/kwuellner/google-book-search/static/js/2.c158c955.chunk.js"
+  },
+  {
+    "revision": "b3a5d5d7e01829a9ac40",
+    "url": "/kwuellner/google-book-search/static/js/main.71c08b2e.chunk.js"
+  },
+  {
+    "revision": "1b8e403bf89424403f5f",
+    "url": "/kwuellner/google-book-search/static/js/runtime-main.2f809b26.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/kwuellner/google-book-search/static/media/logo.25bf045c.svg"
+  }
+]);
